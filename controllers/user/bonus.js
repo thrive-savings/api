087cd17bc6f5631b51265057839f3fed263ad37d
nbaghiro <naib.baghirov@gmail.com>
2018-08-01 11:45:08 -0400

    bonus seen fix

--- controllers/user/bonus.js
+++ controllers/user/bonus.js
@@ -1,9 +1,9 @@
-module.exports = (Bluebird, moment, User, Bonus, Company) => ({
+module.exports = (Bluebird, moment, User, Bonus, Account, Goal, Company) => ({
   notificationSeen: {
     async method (ctx) {
       await Bonus.update({ notificationSeenDate: moment() }, { where: { userID: ctx.authorized.id } })
 
-      const user = await User.findOne({ include: [Company], where: { id: ctx.authorized.id } })
+      const user = await User.findOne({ include: [Account, Goal, Company], where: { id: ctx.authorized.id } })
       const authorizedData = user.getAuthorized()
       authorizedData.notifications.bonus = 0
 
