937552b91325a046ac833ea0d286b151ce20a2e8
nbaghiro <naib.baghirov@gmail.com>
2018-08-16 15:44:03 -0400

    depending on Naib's npm packages instead of rocketstation outsourcers

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const koaStatic = require('koa-static')
-const { serve } = require('rocketstation-api')
+const { serve } = require('api-boilerplate')
 
 const port = process.env.port || 3000
 
 const run = async () => {
   const { app, routes, server } = await serve()
   console.log('Routes:')
-  Object.keys(routes).forEach((item) => { console.log(`${item}: ${routes[item]}`) })
+  Object.keys(routes).forEach(item => {
+    console.log(`${item}: ${routes[item]}`)
+  })
   app.use(koaStatic('assets'))
   server.listen(port)
 }
 
-run().catch((error) => {
+run().catch(error => {
   if (error.stack) console.log(error.stack)
   else console.log(error)
 })
