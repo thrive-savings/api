bb69eeb313e9c5b6b0f0447b94bf9d4dd1603d0d
nbaghiro <naib.baghirov@gmail.com>
2018-10-22 10:44:30 -0400

    ssl in

--- config/production/constants.js
+++ config/production/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  URL: 'http://api.thrivesavings.com',
-  CLIENT_URL: 'http://join.thrivesavings.com',
+  URL: 'https://api.thrivesavings.com',
+  CLIENT_URL: 'https://join.thrivesavings.com',
   SLACK_API_URL: 'https://slack.com/api',
   API_LAYER_URL: 'https://apilayer.net/api/check'
 }
